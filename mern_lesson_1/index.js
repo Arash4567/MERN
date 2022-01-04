a7d4ed58ec457a4e7d0f2ccd13d2c684938e6d0f
Arash4567 <shomoyxhacker007@mail.ru>
2022-01-04 10:50:38 +0300

    lesson 1

---
+++ mern_lesson_1/index.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+const matn = fs.readFileSync('./index.html', 'utf-8');
+console.log(matn);
+
+// TODO: 1 dan 10 gacha bo`lgan sonlarni natija.txt 
+// fayliga yozib beruvchi kod yozing!
+
+// TODO: name.txt fileni yarating va uning ichida bir 
+// nechta ismlar "," bilan ajratib yozilgan bo`lsin!
+// message.txt file da esa ixtiyoriy bir matn bo`lsin! 
+// Vazifangiz har bir ism uchun alohida message fileni 
+// send papkasida yarating!
+
