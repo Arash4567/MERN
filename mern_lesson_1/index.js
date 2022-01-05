8600ff604e17c23b7e10df4676cf26fa8fa58482
Arash4567 <shomoyxhacker007@mail.ru>
2022-01-05 15:16:53 +0500

    minor bug fixed

--- mern_lesson_1/index.js
+++ mern_lesson_1/index.js
@@ -1,20 +1,19 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const matn = fs.readFileSync('./index.html', 'utf-8');
-console.log(matn);
+// const matn = fs.readFileSync('./index.html', 'utf-8');
+// console.log(matn);
 
-// TODO: 1 dan 10 gacha bo`lgan sonlarni natija.txt 
+// TODO: 1 dan 10 gacha bo`lgan sonlarni natija.txt
 // fayliga yozib beruvchi kod yozing!
 
-// TODO: name.txt fileni yarating va uning ichida bir 
+// TODO: name.txt fileni yarating va uning ichida bir
 // nechta ismlar "," bilan ajratib yozilgan bo`lsin!
-// message.txt file da esa ixtiyoriy bir matn bo`lsin! 
-// Vazifangiz har bir ism uchun alohida message fileni 
+// message.txt file da esa ixtiyoriy bir matn bo`lsin!
+// Vazifangiz har bir ism uchun alohida message fileni
 // send papkasida yarating!
 
 // Homework| pupil.txt nomli o`quvchilar ro`yhati bor
 // shu ro`yhatni 2 qismga bo`ling! boys.txt va girls.txt
 // boys.txt fileda faqat o`g`il bolalar,
 // girls.txt da faqat qiz bolalar ro`yhati bo`lsin!
-
 
