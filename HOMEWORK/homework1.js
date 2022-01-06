c4f73da20b977348bcf8d3d5e36c0b2a5aa6a594
Arash4567 <shomoyxhacker007@mail.ru>
2022-01-06 12:59:39 +0300

    Homework added

---
+++ HOMEWORK/homework1.js
@@ -0,0 +1,13 @@
+/*
+1. Eng boy odamni toping!
+2. Eng boy odam kambag`alidan qancha boyroq!
+3. Yoshi 30 dagi odamning do`stlarini topish!
+4. Ko`zi ko`k erkakning nechta do`sti bor!
+5. Eng ko`p do`sti bor odam qayerda yashaydi?
+6. Eng ko`p do`sti bor odamning yoshi?
+7. Olma kimga yoqmaydi?
+8. Eng ko`p o`qilmagan xabar kimga kelgan?
+9. Eng ko`p o`qilmagan xabar qaysi pochtaga kelgan?
+10. Input maydoniga kiritilgan odam haqida 
+ma`lumotni chiqarib beruvchi web ilova yarating!
+*/
