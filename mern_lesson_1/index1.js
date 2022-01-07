5303f8f9df379bf9d7939dbf7eb77233523f261b
Arash4567 <shomoyxhacker007@mail.ru>
2022-01-07 15:34:49 +0300

    minor changes

--- mern_lesson_1/index1.js
+++ mern_lesson_1/index1.js
@@ -1,2 +1,13 @@
 const fs = require('fs');
-fs.readFileSync('./pupil.txt', 'utf-8;', )
+fs.readFile('./pupil.txt', 'utf-8', (err, peoples) => {
+    peoples.split(",").forEach(
+        (inson) => {
+            let human = inson.split(" ")[0].replace('\r\n', '');
+            let sms = fs.readFileSync('./bulkSms.txt', 'utf-8');
+            fs.writeFileSync(`./SEND/${human}.txt`, sms.replace('[NAME]', human));
+        }
+    );
+});
+
+const arr = ['34.5', "4.2", "7.6", "5.2", "8.6", "4.7"];
+console.log(arr.map((value, index) => value * index));
